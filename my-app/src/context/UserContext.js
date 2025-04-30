127f9630f9bca701af12ea5aa3473f45ab7e28bf
Gnurias <gnxrias@gmail.com>
2025-04-30 17:02:36 +0700

    Add files via upload

---
+++ my-app/src/context/UserContext.js
@@ -0,0 +1,17 @@
+// src/context/UserContext.js
+import { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
+
